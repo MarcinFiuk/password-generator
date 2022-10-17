b62e6e37e40869617c53957c38a7acf189ae5374
Marcin_Fi <fiukmarcinn@gmail.com>
2022-10-17 15:30:22 +0100

    feat: adds heading and test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
     return (
         <div className='grid place-items-center h-screen min-h-fit'>
-            AAAAAAA
+            <h1 className='text-clrAccent300 text-base text-center md:text-2xl'>
+                Password Generator
+            </h1>
+            {/*nextStep: here container for output*/}
+            {/*nextstep: here form for choosing strength of the password */}
         </div>
     );
 }
